0a5a45007cb12b7f28dba2a5fbf7634a43fc7658
Obed Chavez <chavezobed35@gmail.com>
2025-10-26 20:02:21 -0400

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -1,16 +1,3 @@
-import { neon } from '@netlify/neon';
-
-const sql = neon('postgresql://neondb_owner:npg_iQJB0qYwU4RC@ep-crimson-mode-ae1rzfg5-pooler.c-2.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require');
-
-async function probarConexion() {
-  try {
-    const result = await sql`SELECT NOW()`;
-    console.log('✅ Conexión exitosa:', result);
-  } catch (err) {
-    console.error('❌ Error en la conexión:', err);
-  }
-}
-
 $(document).ready(function() {
   // Mostrar alerta si viene ?inscrito=1 en la URL
   // Menú hamburguesa para móviles
